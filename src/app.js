d356a21794accb4e6ae799b371ef47ba6c4ba498
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-04-17 00:10:20 +0000

    new version

--- src/app.js
+++ src/app.js
@@ -1,51 +1,54 @@
-import "bootstrap";
+
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
 
- // Possible card values and suits
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
-const suits = ["♠", "♥", "♦", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  const suits = ["♠", "♥", "♦", "♣"];
 
-// Randomly select a value and a suit
-const randomValue = values[Math.floor(Math.random() * values.length)];
-const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const generateCard = () => {
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
 
-// Get card elements
-// const card = document.querySelector(".card");
-const topSuit = document.querySelector(".top-suit");
-const value = document.querySelector(".value");
-const bottomSuit = document.querySelector(".bottom-suit");
 
-// Update card content
-topSuit.textContent = randomSuit;
-value.textContent = randomValue;
-bottomSuit.textContent = randomSuit;
+    const topSuit = document.querySelector(".top-suit");
+    const value = document.querySelector(".value");
+    const bottomSuit = document.querySelector(".bottom-suit");
 
-// Add red color for hearts and diamonds
-if (randomSuit === "♥" || randomSuit === "♦") {
-  topSuit.classList.add("red");
-  value.classList.add("red");
-  bottomSuit.classList.add("red");
-}
 
-  
-};
+    topSuit.textContent = randomSuit;
+    value.textContent = randomValue;
+    bottomSuit.textContent = randomSuit;
+
+
+    if (randomSuit === "♥" || randomSuit === "♦") {
+      topSuit.classList.add("red");
+      value.classList.add("red");
+      bottomSuit.classList.add("red");
+    }
+    
+    generateCard();
+    const button = document.getElementById("refresh");
+    button.addEventListener("click", generateCard);
+
+  };
+
+
+
+
